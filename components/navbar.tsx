741464fe8db31723e8793f0f8f261f27e691c121
Alejandro Gonzalez <pegasus.pixels.ux@gmail.com>
2026-09-26 15:55:39 -0300

    Navbar: change mobile padding to 12px (px-3)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -46,7 +46,7 @@ export function Navbar() {
         }`}
       />
 
-      <nav className="mx-auto flex h-18 max-w-7xl items-center justify-between px-6 lg:px-8">
+      <nav className="mx-auto flex h-18 max-w-7xl items-center justify-between px-3 sm:px-6 lg:px-8">
         <Link
           href="/"
           className={`text-[2rem] leading-none tracking-tight transition-colors duration-200 [font-family:var(--font-script)] ${
